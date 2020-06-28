6bc36c8fb39c09a81f10d6fe9bb4e73fb9505244
Rutger Wessels <rutger@rutgerwessels.nl>
2020-06-28 21:04:29 +0200

    Stub Client calls

--- spec/sma_api/client_spec.rb
+++ spec/sma_api/client_spec.rb
@@ -37,23 +37,42 @@ RSpec.describe SmaApi::Client do
     end
   end
 
-  describe '#sid', :vcr do
+  describe '#sid' do
     subject { client.sid }
+
+    before do
+      allow_any_instance_of(SmaApi::Http).to receive(:sid).and_return('6UBJRT24Yog4xR2C')
+    end
 
     it { is_expected.to match(/\w{16}/) }
   end
 
-  describe '#destroy_session', :vcr do
-    let!(:sid) { client.sid }
+  describe '#destroy_session' do
+    before do
+      allow_any_instance_of(SmaApi::Http).to receive(:destroy_session)
+    end
 
-    subject { client.sid }
-
-    before { client.destroy_session }
-
-    it { is_expected.not_to eq(sid) }
+    it 'calls SmaApi::Http.destroy_session' do
+      expect_any_instance_of(SmaApi::Http).to receive(:destroy_session)
+      client.destroy_session
+    end
   end
 
-  describe '#get_values', :vcr do
+  describe '#get_values' do
+    let(:client_response) do
+      {
+        'result' => {
+          '0199-B32F8CCE' => {
+            '6100_40263F00' => {
+              '1' => [{ 'val' => 865 }]
+            },
+            '6400_00260100' => {
+              '1' => [{ 'val' => 836_990 }]
+            }
+          }
+        }
+      }
+    end
     let(:keys) { %w[6100_40263F00 6400_00260100] }
     let(:result) do
       {
@@ -62,14 +81,58 @@ RSpec.describe SmaApi::Client do
       }
     end
 
+    before do
+      allow_any_instance_of(SmaApi::Http)
+        .to receive(:post)
+        .with('/dyn/getValues.json', { destDev: [], keys: keys })
+        .and_return(client_response)
+    end
+
     subject { client.get_values keys }
 
     it { is_expected.to eq(result) }
   end
 
-  describe '#object_metadata', :vcr do
+  describe '#object_metadata' do
     let(:first_result) do
       %w[Prio TagId TagIdEvtMsg]
+    end
+    let(:client_response) do
+      {
+        "6180_08419000": {
+          'Prio' => 2,
+          'TagId' => 814,
+          'TagIdEvtMsg' => 10_003,
+          'DataFrmt' => 18,
+          'Typ' => 1,
+          'WriteLevel' => 5,
+          'TagHier' => [
+            830,
+            309,
+            3409
+          ]
+        },
+        '6180_08414D00' => {
+          'Prio' => 2,
+          'TagId' => 240,
+          'TagIdEvtMsg' => 11_177,
+          'DataFrmt' => 18,
+          'Typ' => 1,
+          'WriteLevel' => 5,
+          'TagHier' => [
+            830,
+            309,
+            46
+          ]
+        }
+      }
+    end
+
+    before do
+      allow_any_instance_of(SmaApi::Http)
+        .to receive(:post)
+        .with('/data/ObjectMetadata_Istl.json')
+        .and_return(client_response)
     end
 
     subject { client.object_metadata.first[1].keys }
